a9019b4664086fdec004056cb3810bb3382200c9
Laryssa do Amaral de Oliveira <163066504+laryssaamaral@users.noreply.github.com>
2024-05-06 21:15:57 -0300

    print(info)

--- Laryssa.py
+++ Laryssa.py
@@ -4,8 +4,19 @@ import pandas as pd
 
 empresa={'Nomes':['Ana','Nicole','Pedro','Joana','Quibe','Uriel','Ezequias','Kesya','Angel','Samuel'],
          'Idade':[19,23,12,34,65,89,15,17,17,18],
-         'Departamento':['producao','aprediz','vendedor','producao','producao','aprendiz','repositor','aprendiz','desenvolvedor','vendedor']}
+         'Departamento':['producao','aprediz','vendedor','producao','producao','aprendiz','repositor','aprendiz','desenvolvedor','vendedor'],
+         'Salario':[1300,800,1200,1300,1300,800,1900,900,3200,1200]}
 
 
 df=pd.DataFrame(empresa)
 print(df)
+
+
+#Exiba as primeiras linhas do DataFrame usando head() para entender a estrutura dos dados.
+
+print(df.head())
+
+
+#Utilize info() para obter informações sobre as colunas e tipos de dados.
+
+print(df.info())
